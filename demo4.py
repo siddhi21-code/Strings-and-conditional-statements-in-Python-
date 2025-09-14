ef53c2cce31e7c26647c2e2a25d54091f1e451a0
siddhi21-code <siddhishirke508@gmail.com>
2025-09-14 13:35:11 +0530

    add

---
+++ demo4.py
@@ -0,0 +1,58 @@
+str1 = "this is string "
+str2 = "siddhi"
+str3 = """  sidd """
+print(str1)
+print(str2)
+print(str3)
+
+#Concatenation 
+str4 ="my name is"
+str5 ="siddhi"
+print(str4+str5)
+
+#length and str
+str8 ="siddhi"
+str9 ="shirke"
+print(len(str8))
+print(len(str9))
+
+#indexing 
+str ="my faav car thar "
+ch =str [0]
+
+
+#String Function
+str11 = "Hi my name is siddhi "
+print(str11.endswith("dhi"))
+
+
+str34 ="my car thar"
+print(str34.capitalize())
+
+str22 ="i am learn python"
+print(str22.replace("python","java"))
+
+
+str33 ="i love my car thar "
+print(str33.find("o"))
+
+str23 ="coding is good "
+print(str23.count("good"))
+
+#length print ex 
+str12 ="my name sidd"
+print(len(str12))
+
+
+#conditional stratements
+age = 21
+if(age>18):
+       print("can vote & apply  for license")
+
+       #if else
+
+       age = 25
+if age >= 18:
+    print("You are eligible to vote")
+else:
+    print("You are not eligible to vote")
